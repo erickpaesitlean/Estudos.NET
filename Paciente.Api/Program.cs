3cb5e2916751ed50769fddd0fc27f1979a2d77a5
erick.paes <erick.paes@itlean.com.br>
2026-01-25 21:04:34 -0300

    atts...

--- Paciente.Api/Program.cs
+++ Paciente.Api/Program.cs
@@ -6,24 +6,20 @@ using Paciente.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 🔹 Controllers & Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 🔹 DbContext (InMemory)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-// 🔹 DI
 builder.Services.AddScoped<IPacienteRepository, PacienteRepository>();
 builder.Services.AddScoped<PacienteService>();
 
 var app = builder.Build();
 
-// 🔹 Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ Paciente.Domain/Entities/Convenio.cs
@@ -0,0 +1,19 @@
+﻿namespace Paciente.Domain.Entities
+{
+    public class ConvenioEntity
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public int Codico { get; private set; }
+        public  ICollection<PacienteEntity> Pacientes { get; private set; }
+
+
+        public ConvenioEntity(string nome, int codico, ICollection<PacienteEntity> pacientes)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Codico = codico;
+            Pacientes = pacientes;
+        }
+    }
+}
